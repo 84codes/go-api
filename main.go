58d4a57cc78b465dbb4afcf3138f7f633772c58c
Tobias Brodén <tobias@84codes.com>
2019-07-04 11:00:37 +0200

    WIP: Extend API wrapper

--- main.go
+++ main.go
@@ -5,3 +5,12 @@ import "github.com/84codes/go-api/api"
 func NewApi(baseUrl, apiKey string) *api.API {
 	return api.New(baseUrl, apiKey)
 }
+
+func NewCustomerApi(baseUrl, apiKey string) *api.CustomerAPI {
+	return api.New(baseUrl, apiKey)
+}
+
+func NewAlarmApi(baseUrl, apiKey string) *api.AlarmAPI {
+	return api.New(baseUrl, apiKey)
+}
+
