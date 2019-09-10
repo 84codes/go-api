5c522751ffe4e226c8543344ebc38297605f9b06
Tobias Brodén <tobias@84codes.com>
2019-09-10 09:58:08 +0200

    Cleaned up earlier implementation

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
-import "github.com/84codes/go-api/api"
+import (
+	"github.com/84codes/go-api/api"
+)
 
-func NewApi(baseUrl, apiKey string) *api.API {
+func main() {
+
+}
+
+func New(baseUrl, apiKey string) *api.API {
 	return api.New(baseUrl, apiKey)
 }
-
-func NewCustomerApi(baseUrl, apiKey string) *api.CustomerAPI {
-	return api.NewCustomerApi(baseUrl, apiKey)
-}
-
-func NewAlarmApi(baseUrl, apiKey string) *api.AlarmAPI {
-	return api.NewAlarmApi(baseUrl, apiKey)
-}
-
