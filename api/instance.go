011fa6cee8080f9e3c865ea349cca47b8d6d55ae
Tobias Brodén <tobias@84codes.com>
2023-03-30 18:47:56 +0200

    Update naming for wait function

--- api/instance.go
+++ api/instance.go
@@ -56,35 +56,34 @@ func (api *API) waitUntilAllNodesReady(instanceID string) error {
 	}
 }
 
-func (api *API) waitUntilAllNodesReadyWithTimeout(instanceID string, attempt, sleep, timeout int) error {
+func (api *API) waitWithTimeoutUntilAllNodesConfigured(instanceID string, attempt, sleep, timeout int) error {
 	var (
 		data   []map[string]interface{}
 		failed map[string]interface{}
 		path   = fmt.Sprintf("api/instances/%v/nodes", instanceID)
 	)
-	log.Printf("[DEBUG] go-api::instance::waitUntilAllNodesReadyWithTimeout not yet ready, "+
+	log.Printf("[DEBUG] go-api::instance::waitWithTimeoutUntilAllNodesConfigured not yet ready, "+
 		" will try again, attempt: %d, until timeout: %d", attempt, (timeout - (attempt * sleep)))
 
 	_, err := api.sling.New().Path(path).Receive(&data, &failed)
 	if err != nil {
-		log.Printf("[ERROR] go-api::instance::waitUntilAllNodesReadyWithTimeout error: %v", err)
 		return err
 	} else if attempt*sleep > timeout {
-		return fmt.Errorf("Wait until all nodes ready reached timeout of %d seconds", timeout)
+		return fmt.Errorf("All nodes configured timeout reached after %d seconds", timeout)
 	}
 
 	ready := true
 	for _, node := range data {
-		log.Printf("[DEBUG] go-api::instance::waitUntilAllNodesReadyWithTimeout ready: %v, configured: %v", ready, node["configured"])
+		log.Printf("[DEBUG] go-api::instance::waitWithTimeoutUntilAllNodesConfigured ready: %v, configured: %v", ready, node["configured"])
 		ready = ready && node["configured"].(bool)
 	}
-	log.Printf("[DEBUG] go-api::instance::waitUntilAllNodesReadyWithTimeout ready: %v", ready)
+	log.Printf("[DEBUG] go-api::instance::waitWithTimeoutUntilAllNodesConfigured ready: %v", ready)
 	if ready {
 		return nil
 	}
 	attempt++
 	time.Sleep(time.Duration(sleep) * time.Second)
-	return api.waitUntilAllNodesReadyWithTimeout(instanceID, attempt, sleep, timeout)
+	return api.waitWithTimeoutUntilAllNodesConfigured(instanceID, attempt, sleep, timeout)
 }
 
 func (api *API) waitUntilDeletion(instanceID string) error {
